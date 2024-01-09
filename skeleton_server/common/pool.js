1710f804fe4944a778587874ca26843111eabbab
minsuh <minsuh@kakao.com>
2024-01-09 16:18:07 +0900

    a

--- skeleton_server/common/pool.js
+++ skeleton_server/common/pool.js
@@ -19,7 +19,7 @@ module.exports = function getPool() {
         host: process.env.DB_URL,
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
-        password: process.env.PASSWORD,
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         connectionLimit: 10
     }
